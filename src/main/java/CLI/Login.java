822a7f98e24e7e170a7349d17ba6c08c38c4c1e3
maryam banjo <mitechnigeria@gmail.com>
2025-04-21 17:47:53 +0100

    Refactor: MainMenu has basic functionalities like choosing if you are a user or an agent or you want to exit the application. Feat: Login has a boolean method that takes in username,password,and role

--- src/main/java/CLI/Login.java
+++ src/main/java/CLI/Login.java
@@ -1,4 +1,9 @@
 package CLI;
 
+
+
 public class Login {
+    public static boolean login(String username, String password, String role) {
+        return username.equals(password);
+    }
 }
---
+++ src/main/java/CLI/UserMenu.java
@@ -0,0 +1,7 @@
+package CLI;
+public class UserMenu {
+    public static void show(String username) {
+        System.out.println("\n👤 Welcome, " + username + " (User)");
+        // You can add: Create ticket, View tickets, etc.
+    }
+}
