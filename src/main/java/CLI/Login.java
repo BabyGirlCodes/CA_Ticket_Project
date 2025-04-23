a6080315a24d58e0a827c7b063ff457fb53ab5fb
maryam banjo <mitechnigeria@gmail.com>
2025-04-23 12:46:43 +0100

    Feature(login): added login method for users and agents
    - Checks if username and password match using HashMap
    - Handles both user and agent roles
    - Works with data loaded from text files

--- src/main/java/CLI/Login.java
+++ src/main/java/CLI/Login.java
@@ -1,9 +1,20 @@
 package CLI;
 
+import entities.Agent;
+import entities.User;
+import util.HashMap;
 
 
 public class Login {
-    public static boolean login(String username, String password, String role) {
-        return username.equals(password);
+
+    public static boolean login(String username, String password, String role, HashMap<String, User> users, HashMap<String, Agent> agents) {
+        if (role.equalsIgnoreCase("user")) {
+            User u = users.get(username);
+            return u != null && u.getPassword().equals(password);
+        } else {
+            Agent a = agents.get(username);
+            return a != null && a.getPassword().equals(password);
+        }
     }
 }
+
